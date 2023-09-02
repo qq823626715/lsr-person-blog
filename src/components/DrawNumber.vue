6399562a4d7ed6aab4d125fd6e616b78e45dbc32
LiuShengRong <liusr823626715@163.com>
2023-09-02 21:42:22 +0800

    feat: 翻牌器成功

--- src/components/DrawNumber.vue
+++ src/components/DrawNumber.vue
@@ -2,28 +2,51 @@
  * @Description: 数字翻牌器
 -->
 <script setup>
-import { ref, defineProps } from 'vue'
+import { ref, defineProps, watch, onMounted  } from 'vue'
 const props = defineProps({
-  number: {
-    type: Number,
-    default: 0
+  content: {
+    type: String,
+    default: '',
+    require: true
   }
 })
-const activeNum = ref(props.number)
-function handleClick() {
-  activeNum.value = (activeNum.value + 1) % 10
-}
+const activeContent = ref('')
+const nextContent = ref('乙')
+const underway = ref(false)
+onMounted(() => {
+  activeContent.value = props.content
+})
+
+watch(() => props.content, (newQuestion, oldQuestion) => {
+  nextContent.value = newQuestion
+  underway.value = true
+  const timer = setTimeout(() => {
+    activeContent.value = newQuestion
+    nextContent.value = oldQuestion
+    underway.value = false
+    clearTimeout(timer)
+  }, 190)
+})
 </script>
 <template>
-  <div v-bind="$attrs" class="draw" @click="handleClick">
-    <div class="draw-item draw-static" :data-after="activeNum" :data-before="activeNum + 1"></div>
-    <div class="draw-item draw-dynamic" :data-after="activeNum" :data-before="activeNum + 1"></div>
+  <div v-bind="$attrs" class="draw">
+    <div class="draw-item draw-static" :data-active="activeContent" :data-next="nextContent"></div>
+    <div class="draw-item draw-dynamic" :class="{ 'active': underway }" :data-active="nextContent" :data-next="activeContent"></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@keyframes draw {
+  from {
+    transform: rotateX(0)
+  }
+
+  to {
+    transform: rotateX(180deg);
+  }
+}
 .draw {
-  font-size: 100px;
+  font-size: 60px;
   line-height: 140px;
   font-weight: 600;
   color: #FFFFFF;
@@ -31,8 +54,6 @@ function handleClick() {
   
 }
 .draw-static {
-  border: 1px solid #000000;
-  background-color: #000000;
   display: inline-block;
   width: 80px;
   text-align: center;
@@ -42,50 +63,64 @@ function handleClick() {
   justify-content: center;
   align-items: center;
   &::before {
-    content: '4';
+    content: attr(data-next);
     display: block;
+    width: 80px;
     height: 70px;
     line-height: 140px;
     overflow: hidden;
+    background-color: #000000;  
+    border-radius: 0  0 10px 10px;
   }
   &::after {
-    content: '3';
+    content: attr(data-active);
     display: block;
+    width: 80px;
     height: 70px;
     line-height: 0;
     overflow: hidden;
+    background-color: #000000;
+    border-radius: 10px 10px 0  0;
   }
 }
 .draw-dynamic {
-  height: 70px;
-  width: 80px;
-  text-align: center;
   position: absolute;
   top: 0;
-  background-color: #00FF00;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  width: 80px;
+  height: 70px;
+  line-height: 140px;
+  background: #000000;
+  color: #FFFFFF;
+  text-align: center;
   transform-style: preserve-3d;
   transform-origin: bottom;
+  // transition: all .5S;
+  // transform: rotateX(180deg);
+  border-radius: 0  0 10px 10px;
   &::before {
-    content: '3';
-    display: block;
+    content: attr(data-active);
+    width: 80px;
     height: 70px;
-    line-height: 140px;
-    backface-visibility: hidden;
-    transform-style: preserve-3d;
-  }
-  &::after {
-    content: '4';
-    display: block;
-    height: 70px;
+    top: 0;
     line-height: 0;
+    display: block;
+    position: absolute;
     transform: rotateX(180deg);
     backface-visibility: hidden;
-    transform-style: preserve-3d;
+    overflow: hidden;
+  }
+  &::after {
+    content: attr(data-next);
+    width: 80px;
+    height: 70px;
+    line-height: 140px;
+    display: block;
+    backface-visibility: hidden;
+    overflow: hidden;
+  }
+  &.active {
+    animation: draw 0.2s linear;
   }
 }
+
 </style>
--- src/views/utils-modelus/DrawComponent.vue
+++ src/views/utils-modelus/DrawComponent.vue
@@ -4,10 +4,17 @@
 <script setup>
 import { ref, reactive } from 'vue';
 import DrawNumber from '@/components/DrawNumber.vue'
+const content = ref('甲')
+let index = 0
+const dict = ['甲', '乙', '丙','丁','戊','己','庚','辛','壬','癸' ]
+function handleClick() {
+  index = (index+1) % dict.length
+  content.value = dict[index]
+}
 </script>
 <template>
   <div>
-    <DrawNumber />
+    <DrawNumber :content="content" @click="handleClick"/>
   </div>
 </template>
 
