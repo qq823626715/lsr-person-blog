cf854478ca65e8dbb104f825db85903a47ab7f8e
LiuShengRong <liusr823626715@163.com>
2023-09-02 20:22:33 +0800

    style: 修改

--- src/components/DrawNumber.vue
+++ src/components/DrawNumber.vue
@@ -15,12 +15,77 @@ function handleClick() {
 }
 </script>
 <template>
-  <div v-bind="$attrs" class="draw" :data-after="activeNum+1" :data-before="activeNum" @click="handleClick">
-    <div class="draw-item draw-before" :data-after="activeNum" :data-before="activeNum + 1"></div>
-    <div class="draw-item draw-after" :data-after="activeNum" :data-before="activeNum + 1"></div>
+  <div v-bind="$attrs" class="draw" @click="handleClick">
+    <div class="draw-item draw-static" :data-after="activeNum" :data-before="activeNum + 1"></div>
+    <div class="draw-item draw-dynamic" :data-after="activeNum" :data-before="activeNum + 1"></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.draw {
+  font-size: 100px;
+  line-height: 140px;
+  font-weight: 600;
+  color: #FFFFFF;
+  position: relative;
+  
+}
+.draw-static {
+  border: 1px solid #000000;
+  background-color: #000000;
+  display: inline-block;
+  width: 80px;
+  text-align: center;
+  height: 140px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &::before {
+    content: '4';
+    display: block;
+    height: 70px;
+    line-height: 140px;
+    overflow: hidden;
+  }
+  &::after {
+    content: '3';
+    display: block;
+    height: 70px;
+    line-height: 0;
+    overflow: hidden;
+  }
+}
+.draw-dynamic {
+  height: 70px;
+  width: 80px;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  background-color: #00FF00;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transform-style: preserve-3d;
+  transform-origin: bottom;
+  &::before {
+    content: '3';
+    display: block;
+    height: 70px;
+    line-height: 140px;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+  }
+  &::after {
+    content: '4';
+    display: block;
+    height: 70px;
+    line-height: 0;
+    transform: rotateX(180deg);
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+  }
+}
 </style>
